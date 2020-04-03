b774efd98c314836c171e31cfd910a9b65334e50
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2020-04-04 00:26:02 +0300

    reconnect part 1

--- index.js
+++ index.js
@@ -1,41 +1,105 @@
 const WebSocket = require('ws');
 const http = require('http');
+const crypto = require('crypto');
 const server = http.createServer();
 const wss = new WebSocket.Server({ noServer: true });
 var url = require('url');
-function authenticate(request,cb){
-console.log(1)
-var queryData = url.parse(request.url, true).query;
-var user_id=queryData.user_id
-if(user_id==="no_id"){
+var Db=[]
 
-}else{
-	
+async function find_by_id(id){
+for (var i = 0; i < Db.length; i++) {
+   if(Db[i].id===id){
+    return true
+   }
 }
-exit()
-cb()
+return false
+}
+
+async function generate_id() {
+    const id = new Promise((resolve, reject) => {
+        crypto.randomBytes(16, async function(ex, buffer) {
+            if (ex) {
+                reject("error");
+            }
+            let id = buffer.toString("base64").replace(/\/|=|[+]/g, '')
+            let result = await find_by_id(id) //check if id exists
+            if (!result) {
+                resolve(id);
+            } else {
+                let id_1 = await generate_id()
+                resolve(id_1)
+            }
+        });
+    });
+    return id;
+}
+
+async function authenticate(request,cb){
+let queryData = url.parse(request.url, true).query;
+console.log(queryData)
+let user_id=queryData.user_id
+let found= await find_by_id(user_id)
+let client;
+let err=false
+if(found){
+client=found
+}else{
+let id=await generate_id()
+client={id:id,username:null}
+Db.push(client);
+}
+cb(err,client)
 } 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
+
+
+
+
+const interval = setInterval(function ping() {
+  wss.clients.forEach(function each(ws) {
+    if (ws.isAlive === false){console.log("not responding. killing client");return ws.terminate();}
+    ws.isAlive = false;
+    ws.send('1')
   });
- 
-  ws.send('something');
+}, 5000);
+
+wss.on('connection', function connection(ws) {
+  ws.isAlive = true;
+  ws.on('message', function message(data) {
+  	if(data==='1' || data==="2"){
+  		if(data==="2"){
+         ws.send("2")
+  		}
+  		ws.isAlive = true;
+  		return
+  	}
+  	var msg=JSON.parse(data)
+   	console.log(msg)
+  	// console.log(JSON.stringify(client));
+   //  console.log(`Received message ${msg} from user ${JSON.stringify(client)}`);
+  });
+  ws.on('close', function() {
+    console.log(`${JSON.stringify(ws.data)} user disconnected`)
+  });
 });
 
-server.on('upgrade', function upgrade(request, socket, head) {
+
+
+server.on('upgrade', async function upgrade(request, socket, head) {
   authenticate(request, (err, client) => {
     if (err || !client) {
       socket.destroy();
+      console.log("auth failed")
       return;
     }
-
+    console.log("auth succeeded")
     wss.handleUpgrade(request, socket, head, function done(ws) {
-      wss.emit('connection', ws, request, client);
+      ws.data=client
+      wss.emit('connection', ws);
     });
 
   });
 });
+
 
 server.listen(8080);
 console.log("server started at port 8080")
